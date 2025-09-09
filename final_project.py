e1c41cb16c68c7a0ab5d9d63ba16b32fa949eda9
StephanieGanye <stphnganye@gmail.com>
2025-09-09 22:46:19 +0000

    Initial commit

---
+++ final_project.py
@@ -0,0 +1,459 @@
+from graphics import Canvas
+import math
+import time
+import random
+
+# Constants for canvas size and assumed floor load
+CANVAS_WIDTH = 400
+CANVAS_HEIGHT = 400
+
+FLOOR_WEIGHT = 6
+
+"""
+# Soil property data: [cohesion_min, cohesion_max, 
+phi_min, phi_max, unit_weight_min, unit_weight_max]
+"""
+LOOSE_SAND = [0, 0, 28, 32, 16, 18]
+MEDIUM_DENSE_SAND = [0, 0, 32, 36, 17, 19]
+DENSE_SAND = [0, 0, 36, 40, 18, 21]
+SOFT_CLAY = [15, 25, 0, 5, 14, 17]
+STIFF_CLAY = [40, 75, 0, 10, 17, 20]
+SILTY_SAND = [5, 15, 26, 34, 16, 19]
+GRAVEL = [0, 0, 36, 42, 19, 22]
+
+"""
+Adjust to change cloud size
+A cloud with width that is 2 times its height 
+gives best result
+"""
+CLOUD_WIDTH = 80
+CLOUD_HEIGHT = 40
+
+TRUNK_HEIGHT = 60
+TRUNK_WIDTH = 15
+LEAVES_SIZE = 60
+
+TREE_BOTTOM_Y = CANVAS_HEIGHT - 20 
+# Controls how fast the clouds move in the sky
+DELAY = 0.1
+
+def main():
+    # Create canvas
+    canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
+    """
+    Create scene for building drawing by drawing a light blue rectangle
+    representing the sky and three white clouds
+    """
+    canvas.create_rectangle(0, 0, 400, 120, 'light blue') # Draws sky
+    
+    # Draws three white clouds
+    x_cloud_one = 20 # x-coordinate for first cloud
+    y_cloud_one = 70 # y-coordinate for first cloud
+    cloud_one = draw_cloud(canvas, x_cloud_one, y_cloud_one, "white")
+
+    x_cloud_two = 270 # x-coordinate for second cloud
+    y_cloud_two = 20 # y-coordinate for second cloud
+    cloud_two = draw_cloud(canvas, x_cloud_two, y_cloud_two, "white")
+
+    x_cloud_three = 270 # x-coordinate for third cloud
+    y_cloud_three = 70 # y-coordinate for third cloud
+    cloud_three = draw_cloud(canvas, x_cloud_three, y_cloud_three, "white")
+    
+
+
+    print("Let's see if your dream home will stand strong or sink like the Titanic") 
+    print()
+    print()
+    time.sleep(1) # Brief pause
+   
+    # Ask user to input soil type
+    soil_type = choose_soil_type()
+
+    # Draw soil based on user input
+    draw_soil(canvas, soil_type)
+
+    # Tell user the pros and cons of the soil type they chose
+    properties = pros_and_cons(soil_type)
+
+    # Sort soil type to its full name
+    type_name = sort_type(soil_type)
+
+    # Display selected soil properties and related pros and cons
+    print_variable_guide(soil_characteristics, type_name, properties)
+
+    canvas.wait_for_click()
+
+def draw_cloud(canvas, initial_x, initial_y,color):
+    """
+    This function takes x and y coordinates of the top right
+    corner of the cloud and its color.
+    To create the cloud, it draws three different ovals with the
+    top one being closer to a circle than the bottom 2
+    """
+
+    
+    # To create bottom left oval
+    left_x1 = initial_x 
+    top_y1 = initial_y + CLOUD_HEIGHT/3
+    right_x1 = initial_x + 3 * CLOUD_WIDTH/4
+    bottom_y1 = initial_y + CLOUD_HEIGHT
+    oval1 = canvas.create_oval(left_x1,top_y1,right_x1,bottom_y1,color)
+
+    # To create bottom right oval. 
+    left_x2 = initial_x + CLOUD_WIDTH/4
+    top_y2 = initial_y + CLOUD_HEIGHT/3
+    right_x2 = initial_x + CLOUD_WIDTH
+    bottom_y2 = initial_y + CLOUD_HEIGHT
+    oval2 = canvas.create_oval(left_x2,top_y2,right_x2,bottom_y2,color)
+
+    # Create top oval
+    left_x3 = initial_x + CLOUD_WIDTH/4
+    top_y3 = initial_y 
+    right_x3 = initial_x + 3 * CLOUD_WIDTH/4
+    bottom_y3 = initial_y + 3 * CLOUD_HEIGHT/4
+    oval3 = canvas.create_oval(left_x3,top_y3,right_x3,bottom_y3,color)
+
+    return(oval1, oval2, oval3)
+
+def animate_clouds(canvas, cloud_one, cloud_two, cloud_three):
+    # move created clouds around the sky to create a more engaging environment
+    speed = DELAY
+    # Track change in x and y direction
+    x1_change = 1
+    y1_change = -1
+
+    # Track change in y direction for second cloud
+    y2_change = 1
+    X2_change = [-250, 250]
+
+    # Track change in x direction for third cloud
+    x3_change = 1.5
+
+    while True:
+        # Move first cloud diagonally
+        if canvas.get_top_y(cloud_one[2]) == -90:
+            x1_change = -x1_change
+            y1_change = -y1_change
+        elif canvas.get_top_y(cloud_one[2]) == 150:
+            x1_change = -x1_change
+            y1_change = -y1_change
+
+        for cloud_obj in cloud_one:
+            canvas.move(cloud_obj, x1_change, y1_change)
+
+        # Move second cloud downward and upward
+        if canvas.get_top_y(cloud_two[2]) == 120:
+            if canvas.get_left_x(cloud_two[0]) == 270:
+                for cloud_obj in cloud_two:
+                    canvas.move(cloud_obj, X2_change[0], -190)
+            else:
+                for cloud_obj in cloud_two:
+                    canvas.move(cloud_obj, X2_change[1], -190)
+        else:
+            for cloud_obj in cloud_two:
+                canvas.move(cloud_obj, 0, y2_change)
+
+        # Move third cloud sideways
+        if canvas.get_left_x(cloud_three[0]) == 600:
+            for cloud_obj in cloud_three:
+                canvas.move(cloud_obj, -1200, 0)
+        else:
+            for cloud_obj in cloud_three:
+                canvas.move(cloud_obj, x3_change, 0)
+        time.sleep(speed)
+
+# Gives user soil types and prompts user to choose one
+def choose_soil_type():
+    # Let user choose soil type   
+    print("What soil type would you prefer to build on?"
+    "\n1.Loose Sand (LS)"
+    "\n2. Medium Dense Sand (MDS)"
+    "\n3. Dense Sand (DS)"
+    "\n4. Soft Clay (SC)"
+    "\n5. Stiff Clay (SIC)"
+    "\n6. Silty Sand (SS)"
+    "\n7. Gravel (G)"
+    )
+    soil_type = input("Input soil type initial here: ").upper()
+    print()
+
+    # Ensure user chooses valid soil type
+    while soil_type not in ["LS", "MDS", "DS", "SC", "SIC", "SS", "G"]:
+        soil_type = input("Kindly input valid soil type (LS, MDS, DS, SC, SIC, SS, G): ").upper()
+        print()
+
+    return soil_type
+"""
+Takes canvas and soil type choosen as inputes
+Based on soil type choosen, it draws a rectangle with a unique color
+and objects representing soil particles
+"""
+def draw_soil(canvas, soil_type):
+    particle_size = 10
+    initial_x = 0
+    initial_y = 3 * CANVAS_HEIGHT/4
+    color = 'deep brown'
+
+    
+    # Draw soil
+    soil = canvas.create_rectangle(0, 3 * CANVAS_HEIGHT/4,
+                                    CANVAS_WIDTH,
+                                    CANVAS_HEIGHT, 'beige')
+    
+    if soil_type == "LS":
+        while (initial_y + particle_size) <= CANVAS_HEIGHT:
+            while (initial_x + particle_size) <= CANVAS_WIDTH:
+
+                soil_particle = canvas.create_oval(initial_x,
+                                        initial_y,
+                                        initial_x + particle_size,
+                                        initial_y + particle_size,
+                                        color)
+
+                spacing = random.randint(1,25)
+                # Update initial_x to begin at specified point on right 
+                initial_x += (particle_size + spacing)
+
+            spacing = 1
+            # Update initial_y to begin below
+            initial_y += (particle_size + spacing)
+            initial_x = 0
+    
+    
+    elif soil_type == "MDS":
+        while (initial_y + particle_size) <= CANVAS_HEIGHT:
+            while (initial_x + particle_size) <= CANVAS_WIDTH:
+
+                soil_particle = canvas.create_oval(initial_x,
+                                        initial_y,
+                                        initial_x + particle_size,
+                                        initial_y + particle_size,
+                                        color)
+
+                spacing = random.randint(10, 25)
+                # Update initial_x to begin at specified point on right 
+                initial_x += (particle_size + spacing)
+
+            spacing = 5
+            # Update initial_y to begin below
+            initial_y += (particle_size + spacing)
+            initial_x = 0
+    
+    elif soil_type == "DS":
+        while (initial_y + particle_size) <= CANVAS_HEIGHT:
+            while (initial_x + particle_size) <= CANVAS_WIDTH:
+
+                soil_particle = canvas.create_oval(initial_x,
+                                        initial_y,
+                                        initial_x + particle_size,
+                                        initial_y + particle_size,
+                                        color)
+
+                spacing = 5
+                # Update initial_x to begin at specified point on right 
+                initial_x += (particle_size + spacing)
+
+            spacing = 5
+            # Update initial_y to begin below
+            initial_y += (particle_size + spacing)
+            initial_x = 0
+    
+    elif soil_type == "SC":
+        while (initial_y + particle_size) <= CANVAS_HEIGHT:
+            while (initial_x + particle_size) <= CANVAS_WIDTH:
+                particle_size_x = random.randint(3, 10)
+                particle_size_y = 1
+
+                soil_particle = canvas.create_oval(initial_x,
+                                        initial_y,
+                                        initial_x + particle_size_x,
+                                        initial_y + particle_size_y,
+                                        'beige')
+
+                spacing = random.randint(20, 30)
+                # Update initial_x to begin at specified point on right 
+                initial_x += (particle_size + spacing)
+
+            spacing = 15
+            # Update initial_y to begin below
+            initial_y += (particle_size + spacing)
+            initial_x = 0
+   
+    elif soil_type == "SIC":
+        while (initial_y + particle_size) <= CANVAS_HEIGHT:
+            while (initial_x + particle_size) <= CANVAS_WIDTH:
+                particle_size_x = 6
+                particle_size_y = 3
+
+                soil_particle = canvas.create_rectangle(initial_x,
+                                        initial_y,
+                                        initial_x + particle_size_x,
+                                        initial_y + particle_size_y,
+                                        'beige')
+
+                spacing = random.randint(20, 25)
+                # Update initial_x to begin at specified point on right 
+                initial_x += (particle_size + spacing)
+
+            spacing = 10
+            # Update initial_y to begin below
+            initial_y += (particle_size + spacing)
+            initial_x = random.randint(0, 20)
+   
+    elif soil_type == "SS":
+        while (initial_y + particle_size) <= CANVAS_HEIGHT:
+            while (initial_x + particle_size) <= CANVAS_WIDTH:
+                particle_size_x = random.randint(1, 7)
+                particle_size_y = 1
+
+                soil_particle = canvas.create_oval(initial_x,
+                                        initial_y,
+                                        initial_x + particle_size_x,
+                                        initial_y + particle_size_y,
+                                        'beige')
+
+                spacing = random.randint(20, 40)
+                # Update initial_x to begin at specified point on right 
+                initial_x += (particle_size + spacing)
+
+            spacing = 10
+            # Update initial_y to begin below
+            initial_y += (particle_size + spacing)
+            initial_x = random.randint(0, 20)
+    
+    else:
+        while (initial_y + particle_size) <= CANVAS_HEIGHT:
+            while (initial_x + particle_size) <= CANVAS_WIDTH:
+                particle_size_poly = random.randint(9,15)
+                polygon_top_left = initial_x + 3
+                polygon_top_right = initial_x + random.randint(7,10)
+                polygon_middle_right = initial_x + random.randint(11,15)
+                polygon_bottom_left = initial_x + random.randint(3,5)
+                polygon_bottom_right = initial_x + random.randint(7,10)
+                particle_size_x = random.randint(7, 15)
+                particle_size_y = random.randint(7, 15)
+                soil_particle = canvas.create_polygon(polygon_top_left,
+                                                initial_y,
+                                                polygon_top_right,
+                                                initial_y + 2,
+                                                polygon_middle_right,
+                                                initial_y + (1/2 * particle_size_poly),
+                                                polygon_bottom_right,
+                                                initial_y + particle_size_poly,
+                                                polygon_bottom_left,
+                                                initial_y + particle_size_poly,
+                                                initial_x,
+                                                initial_y + (1/2 * particle_size_poly)
+                                                )
+                spacing = random.randint(0,3)
+                # Update initial_x to begin at specified point on right 
+                initial_x = (polygon_middle_right + spacing) 
+            
+            # Update initial_y to begin below
+            initial_y += (10 + spacing)
+            initial_x = random.randint(0, 3)
+   
+def pros_and_cons(soil_type_choosen):
+    if soil_type_choosen == "LS":
+        pros_and_cons = """
+        Loose sand drains water quickly which reduces pore water pressure
+        Don't feel pressured if you don't know what that is (lame pun intended)
+        It simply means your building will have less forces working against it
+        However be careful, LS has low strength and is prone to settlement
+        """
+    elif soil_type_choosen == "MDS":
+        pros_and_cons = """
+        MDS, much like its name, has medium strength and drainage
+        This is good since sand is known for its low strength
+        MDS also has a better load distribution than LS
+        However, it is prone to settlement which can cause your beautiful home to sink
+        """
+    elif soil_type_choosen == "DS":
+        pros_and_cons = """   
+        Good choice, it has high shear strength, excellent bearing capacity and low compressibility
+        But hey, I hope you aren't planning to live in an earthquake prone zone though
+        Sesmic activity can cause the soil to flow like water and destroy your property
+        The low compressibility also means you can't compact it well to rule out settlement
+        """
+    elif soil_type_choosen == "SC":
+        pros_and_cons = """ 
+        The soft nature of the soil makes it easy to evacuate 
+        But be very careful, SC has very low strength 
+        and keeps settling over long periods of time 
+        Unlike the Titanic, you will have no warning that your building is sinking
+        as it can take years
+        """
+    elif soil_type_choosen == "SIC":
+        pros_and_cons = """
+        Compared to SC, it has a higher bearing strength
+        and reduced settlement risk
+        But watch out, it can develop cracks when dry, 
+        leading to water seepage and volume changes.
+        """
+    elif soil_type_choosen == "SS":
+        pros_and_cons = """
+        SS has moderate bearing capacity if compacted well
+        The cons are, it retains water
+        so I wouldn't recommend it if you live at a snowy place
+        as it becomes prone to frost heave and reduced strength when saturated.
+        """
+    else:
+        pros_and_cons = """
+        Like the Bible said, is is wise to build on a rock 
+        (or several tiny rocks as in the case of gravels)
+        G has a high bearing capacity, excellent drainage and rarely compresses
+        On the other hand it is hard to compact uniformly 
+        and may allow water movement under foundations
+        It is also hard and expensive to excavate but that's not a problem for you
+        We both know Jeff Bezos has got nothing on you 😉
+        """
+    return pros_and_cons
+
+def sort_type(soil_type):
+    # Sort soil properties
+    if soil_type == "LS":
+        soil_characteristics = LOOSE_SAND
+        type_name = "Loose Sand"
+    elif soil_type == "MDS":
+        soil_characteristics = MEDIUM_DENSE_SAND 
+        type_name = "Medium Dense Sand"
+    elif soil_type == "DS":
+        soil_characteristics = DENSE_SAND
+        type_name = "Dense sand"
+    elif soil_type == "SC":
+        soil_characteristics = SOFT_CLAY
+        type_name = "Soft Clay"
+    elif soil_type == "SIC":
+        soil_characteristics = STIFF_CLAY
+        type_name = "Stiff Clay"
+    elif soil_type == "SS":
+        soil_characteristics = SILTY_SAND
+        type_name = "Silty Sand"
+    else:
+        soil_characteristics = GRAVEL
+        type_name = "Gravel"
+
+    return type_name  
+
+def print_variable_guide(soil_characteristics, type_name, properties):
+    print(f"You have selected: {type_name}\n{properties}")
+
+    time.sleep(15) # Pause for 15 seconds
+
+    print(f"Typical property values for {type_name} are: ")
+
+    if type_name in ["Loose Sand", "Gravel", "Medium Dense Sand", "Dense Sand"]:
+        print("Cohesion (c): 0")
+    else:
+        print(f"Cohesion (c): {soil_characteristics[0]} - {soil_characteristics[1]}")
+    print(f"Friction angle (φ): {soil_characteristics[2]} - {soil_characteristics[3]}"
+    f"\nUnit weight (γ): {soil_characteristics[4]} - {soil_characteristics[5]}")
+    print()
+    print("For this program, we will use the median values of the ranges")
+    print()
+    print()
+    time.sleep(5)
+
+
+if __name__ == '__main__':
+    main()
